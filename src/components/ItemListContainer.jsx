5c28a2e4893207d92fcddfad7f3a77ba547c4fd2
Mariángeles Somma <angie.somma@MacBook-Pro-de-Tomas.local>
2025-02-08 18:10:10 -0300

    CreaTuLanding+Somma

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,3 +1,9 @@
+import PropTypes from 'prop-types';
+
 export const ItemListContainer = (props) => (
   <h1 className="mt-5"> {props.saludo} </h1>
 );
+
+ItemListContainer.propTypes = {
+  saludo: PropTypes.string.isRequired,
+};
